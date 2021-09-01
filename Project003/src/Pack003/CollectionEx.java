0522fcce2fa6271eb96e9a7774d7fea2731493b3
jueun-cp <jueun.cp@gmail.com>
2021-09-01 13:06:13 +0900

    [Java] collection-(1)list

---
+++ Project003/src/Pack003/CollectionEx.java
@@ -0,0 +1,20 @@
+package Pack003;
+
+import java.util.*;
+
+public class CollectionEx {
+
+	public static void main(String[] args) {
+		ArrayList<String> arrList = new ArrayList<String>();
+		
+		arrList.add("넷");
+		arrList.add("둘");
+		arrList.add("셋");
+		arrList.add("하나");
+		
+		for (String value : arrList) {
+			System.out.println(value);
+		}
+	}
+
+}
